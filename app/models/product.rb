865027e26c18abe94fc8cd40eda26b94d618a50c
You Matsumoto <youmts22@gmail.com>
2019-08-04 17:04:00 +0900

    Producetの検証を追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
 
-  validates :title, presence: true
-  validates :price, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :image, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end
